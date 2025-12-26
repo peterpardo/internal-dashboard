78fa7441970f7d24d6e92cdd80d30d754f5004e7
peterpardo <peterpardo123@gmail.com>
2025-12-26 19:25:59 +0800

    chore: update viewport implementation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -21,6 +21,11 @@ export const metadata: Metadata = {
     initialScale: 1,
   },
 };
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale:1
+}
 
 export default function RootLayout({
   children,
