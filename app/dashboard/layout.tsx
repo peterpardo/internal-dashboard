6c3cfb2b5480e4c9a5a533517011e1e4b778ee25
peterpardo <peterpardo123@gmail.com>
2025-12-26 17:15:23 +0800

    feat: initial components

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,10 @@
+export default function DashboardLayout() {
+  /**
+   * Dashboard layout component
+   * 1. Sidebar
+   * 2. Header
+   * 3. Main content area
+   * 
+   */
+  return <div>DashboardLayout</div>;
+}
