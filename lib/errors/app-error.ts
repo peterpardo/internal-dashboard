1bce6cfd4ea2fd04587b78477ed21e84b77d85a2
peterpardo <peterpardo123@gmail.com>
2026-01-12 23:55:56 +0800

    chore: update status type to number

--- lib/errors/app-error.ts
+++ lib/errors/app-error.ts
@@ -1,7 +1,7 @@
 export class AppError extends Error {
   constructor(
     public code: string,
-    public status: string,
+    public status: number,
     message: string,
   ) {
     super(message);
