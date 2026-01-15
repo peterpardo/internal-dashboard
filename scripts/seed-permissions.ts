938c2e1ec94912109e28082b1b7d165c0ec4373d
peterpardo <peterpardo123@gmail.com>
2026-01-16 00:31:47 +0800

    chore: added permissions in seeder

--- scripts/seed-permissions.ts
+++ scripts/seed-permissions.ts
@@ -2,34 +2,42 @@ import { pool } from "./db";
 
 export const PERMISSIONS = [
   {
+    id: 1,
     key: "user.read",
     description: "Read user information",
   },
   {
+    id: 2,
     key: "user.create",
     description: "Create new users",
   },
   {
+    id: 3,
     key: "user.update",
     description: "Update existing users",
   },
   {
+    id: 4,
     key: "user.delete",
     description: "Delete users",
   },
   {
+    id: 5,
     key: "service_request.read",
     description: "Read service requests",
   },
   {
+    id: 6,
     key: "service_request.create",
     description: "Create service requests",
   },
   {
+    id: 7,
     key: "service_request.update",
     description: "Update service requests",
   },
   {
+    id: 8,
     key: "service_request.change_status",
     description: "Change service request status",
   },
@@ -39,11 +47,11 @@ async function seedPermissions() {
   for (const perm of PERMISSIONS) {
     await pool.query(
       `
-        INSERT INTO permissions (key, description)
-        VALUES ($1, $2)
+        INSERT INTO permissions (id, key, description)
+        VALUES ($1, $2, $3)
         ON CONFLICT (key) DO NOTHING
-        `,
-      [perm.key, perm.description],
+      `,
+      [perm.id, perm.key, perm.description],
     );
   }
 
